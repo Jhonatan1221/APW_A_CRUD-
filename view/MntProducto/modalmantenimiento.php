2416611bf37187ea6d81d3af91528a0e90f3ceb8
Jhonatan1221 <108557046+Jhonatan1221@users.noreply.github.com>
2022-07-27 18:08:18 -0500

    Add files via upload

---
+++ view/MntProducto/modalmantenimiento.php
@@ -0,0 +1,24 @@
+<div id="modalmantenimiento" class="modal fade bd-example-model" tabindex = "-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+
+<div class="modal-dialog">
+    <div class="modal-content">
+        <div class = "modal-header">
+            <button type="button" class="model-close" data-dimiss="modal" aria-label="close">
+                <i class="font-icon-close-2"></i>
+            </button>
+            <h4 class="modal-title" id="mdtitulo"></h4>
+            <form method="POST" id="producto_form"></form>
+                <div class="modal-body">
+
+                </div>
+                <div class="modal-body">
+                    <button type="button"  class="btn btn-rounded btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="submit"  name="action" id="#" value="add" class="btn btn-rounded btn-primary">Guardar</button>
+
+                </div>
+        </div>
+
+    </div>
+
+</div>
+</div>
